d2bbf99bd9c4fa3e653cdf8aa6823bf18713a949
diegoperesmarques <diegoperesmarques@hotmail.com>
2021-09-10 23:44:50 -0300

    Criação da função para validação do menu principal

--- inicio.py
+++ inicio.py
@@ -1,31 +1,19 @@
 import os
 import cadastroArea
 import formatacoes
+import validacoes
 
 
 while True:
     os.system('cls' if os.name == 'nt' else 'clear')
     formatacoes.titulo('PAGINA INICIAL')
     print('Escolha o que você deseja fazer')
-    print('1 - Cadastrar area')
+    print('1 - Calcular áreas')
     print('2 - Pia (Cozinha)')
     print('999 - Sair')
-    opcao = ' '
+    opcao = validacoes.numeroMenuPrincipal()
 
-    #Validação da opção digitada
-    while True:
-        try:
-            opcao = int(input('Digite a opção válida: '))
-            if (opcao == 1) or (opcao == 999):
-                break
-            formatacoes.titulo('INVÁLIDO!')
-            print('Você digitou uma opção inválida. Tente novamente!')
-
-        except Exception as erro:
-            formatacoes.titulo('INVÁLIDO!')
-            print('Você digitou uma opção inválida. Tente novamente!')
-
-    #Cadastro da cotacao
+    #Calculo da área
     if opcao == 1:
         cadastroArea.cadastro()
 
--- validacoes.py
+++ validacoes.py
@@ -10,3 +10,18 @@ def numeroReal(validando):
             print('Você digitou um número inválido. Tente novamente!')
 
     return numeroResultado
+
+def numeroMenuPrincipal():
+    while True:
+        try:
+            numeroDigitado = int(input('Digite a opção válida: '))
+            if (numeroDigitado == 1) or (numeroDigitado == 999):
+                break
+            formatacoes.titulo('INVÁLIDO!')
+            print('Você digitou uma opção inválida. Tente novamente!')
+
+        except Exception as erro:
+            formatacoes.titulo('INVÁLIDO!')
+            print('Você digitou uma opção inválida. Tente novamente!')
+    
+    return numeroDigitado
