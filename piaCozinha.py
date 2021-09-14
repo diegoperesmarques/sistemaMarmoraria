630357cc3c3645d4ab5d9e0f0e4343e7b7313f82
diegoperesmarques <diegoperesmarques@hotmail.com>
2021-09-13 21:17:24 -0300

    Recebimendo do tipo de pia (chumbada ou apoiada)

--- piaCozinha.py
+++ piaCozinha.py
@@ -11,11 +11,20 @@ def piaReta():
     quantidadeGuanicao = 4 - ladosParede 
     numeroEspelhos = validacoes.numeroEspelhos('Quantos espelhos? ', ladosParede)
 
-    perguntaTamEspelho = input('Espelho terá tamanho padrão? [S/N]').strip().upper()[0]
+    perguntaTamEspelho = input('Espelho terá tamanho padrão [S/N]? ').strip().upper()[0]
     if perguntaTamEspelho == 'N':
         tamanhoEspelho = validacoes.numeroInteiro('Qual será o tamanho do espelho (cm)? ')
     else:
         tamanhoEspelho = 8
+
+    tipoApoioPia = input('A pia é CHUMBADA ou APOIADA? ').upper().strip()
+    if tipoApoioPia == "CHUMBADA":
+        tamanhoApoioPia = 2
+        tamanhoPadrao = input(f'Vai deixar com {tamanhoApoioPia} cm ou será outro valor [S/N]? ').upper().strip()[0]
+        if tamanhoPadrao == "N":
+            tamanhoApoioPia = validacoes.numeroInteiro('Qual o espaçamento da pia com a parede? ')
+    else:
+        tamanhoApoioPia = 0
 
     area = float(largura) * float(profundidade)
 
@@ -27,5 +36,6 @@ def piaReta():
     print(f'Número de espelhos: {numeroEspelhos}')
     print(f'Tamanho do espalho: {tamanhoEspelho}')
     print(f'Quantidade de guanições: {quantidadeGuanicao}')
+    print(f'Espaçamento pia da parede: {tamanhoApoioPia}')
 
     input('Pressione enter para voltar ao Menu anterior')
